cac12c83fac370c78a202bc953a7aeb590fed1b3
Lukas Wais <lukas.wais@outlook.de>
2021-01-07 12:24:15 +0100

    Setup basic JavaFX project.
    Issue: https://github.com/CODERS-BAY/airport/issues/1
    Signed-off-by: Lukas Wais <lukas.wais@jku.at>

---
+++ JavaBackend/src/main/java/codersbay/application/App.java
@@ -0,0 +1,28 @@
+package codersbay.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("view/scene.fxml")));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getClassLoader().getResource("style/style.css")).toExternalForm());
+
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
